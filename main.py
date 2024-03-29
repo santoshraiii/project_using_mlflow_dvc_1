8336583a3526fd6a42e67115bb8d2773c8e3b71f
santosh rai <santoshrai2072001@gmail.com>
2024-03-29 17:41:33 +0530

    data ingestion added

---
+++ main.py
@@ -0,0 +1,13 @@
+from CNNClassifier import logger
+from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "DATA INGESTION STAGE"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
